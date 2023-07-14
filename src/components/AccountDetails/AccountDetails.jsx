8151f48a50c2d40f90cb746ef3568a933085899c
Anita Rostomyan <info@tfnlab.com>
2023-07-14 03:55:00 -0700

    update

---
+++ src/components/AccountDetails/AccountDetails.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid'; // Import the v4 function from the uuid library
+import Web3 from 'web3';
+
+
+
+const AccountDetails = ({ accountAddress, accountBalance }) => {
+  const [value, setValue] = useState('');
+  const [signature, setSignature] = useState('');
+
+  const url = 'https://net.bnetly.com/post.jsp'; // replace with your target URL
+
+  const signMessage = async () => {
+    try {
+      const web3 = new Web3(window.ethereum);
+      const accounts = await web3.eth.requestAccounts();
+      const account = accounts[0];
+      const message = value; // The message you want to sign
+      const signedMessage = await web3.eth.personal.sign(message, account, '');
+      setSignature(signedMessage);
+      return signedMessage;
+    } catch (error) {
+      console.error('Error signing message:', error);
+    }
+  };
+
+  const handleClick = async () => {
+    if (value !== '') {
+      const key = uuidv4();
+      const data = {
+        key: key,
+        value: value,
+        accountAddress: accountAddress
+      };
+
+      // Call the signTransaction function to generate the signature
+      const signature = await signMessage(data);
+
+      // Attach the signature to the payload
+      data.signature = signature;
+
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      };
+
+      // Send the data to the server and get the response
+      const response = await fetch(url, requestOptions);
+      const responseData = await response.text();
+      alert(responseData);
+    } else {
+      console.error('Please enter a value');
+    }
+  };
+
+
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-5">Publish Message</h1>
+        <div class="card col-md-12" >
+          <div class="card-body">
+
+                      <hr className="my-4" />
+                      <p>
+                        Account Address: {accountAddress}
+                      </p>
+                      <hr className="my-4" />
+                      <input type="text" value={value} onChange={e => setValue(e.target.value)} placeholder="Enter message here" style={{width: '100%'}} />
+
+                      <br className="my-2" />
+                      <br className="my-2" />
+                      <button onClick={handleClick}>Publish</button>
+                      <hr className="my-4" />
+
+                                  <input
+                                    type="text"
+                                    value={signature}
+                                    readOnly
+                                    placeholder="Signature"
+                                    style={{ width: '100%' }}
+                                  />
+                      <hr className="my-4" />
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountDetails;
---
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import metamaskIcon from "./metamask.svg";
+import Web3 from 'web3';
+import { v4 as uuidv4 } from 'uuid'; // Import the v4 function from the uuid library
+// import { Entity, Scene } from "aframe-react";
+import "./styles.css";
+
+const ConnectToMetamask = ({ connectToMetamask }) => {
+  const [value, setValue] = useState('');
+
+  const url = 'https://net.bnetly.com/post.jsp'; // replace with your target URL
+
+  const handleClick = () => {
+    if (value !== '') {
+      const key = uuidv4(); // Generate a UUID key using the v4 function
+      const data = { key: key, value: value }; // Include the key and value in the payload
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      };
+
+      fetch(url, requestOptions)
+        .then(response => response.text())
+        .then(data => alert(data))
+        .catch(error => console.error('Error:', error));
+    } else {
+      console.error('Please enter a value');
+    }
+  };
+
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-5">
+          Baby Ripple Inu
+        </h1>
+        Welcome to Our Token: The Ultimate Ripple Flipping Platform!
+        <hr className="my-4" />
+
+        Our mission is simple: to provide you with a seamless and profitable experience in flipping Ripple (XRP). With our platform, you can take advantage of the ever-changing cryptocurrency market and turn your Ripple investments into substantial profits.
+        <hr className="my-4" />
+        <input type="text" value={value} onChange={e => setValue(e.target.value)} placeholder="Enter message here" style={{width: '100%'}} />
+
+        <br className="my-2" />
+        <br className="my-2" />
+        <button onClick={handleClick}>Publish</button>
+
+        <hr className="my-4" />
+        <a href="https://opensea.io/collection/bnet-1" >
+          <img src="https://meta.bnetly.com/images/bnetly.png" width="100%" alt="Bnetly" />
+        </a>
+        <hr className="my-4" />
+        <button
+          onClick={connectToMetamask}
+          className="btn btn-primary d-flex align-items-center"
+          style={{ fontSize: "0.9rem", letterSpacing: "0.14rem" }}
+        >
+          Connect Wallet
+        </button>
+        <hr className="my-4" />
+
+      </div>
+      <hr className="my-4" />
+
+      <div className="container">
+        <hr className="my-4" />
+        bnet &copy; 2023 All rights reserved.
+        <hr className="my-4" />
+      </div>
+    </div>
+  );
+};
+
+export default ConnectToMetamask;
